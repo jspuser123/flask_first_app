00f51bec3fd20cadc634cd7daf7708d797f7e6a7
jspuser123 <126677122+jspuser123@user.noreply.github.com>
2023-04-28 17:33:57 +0530

    error fix

--- static/js/data.js
+++ static/js/data.js
@@ -1,9 +1,10 @@
 $(document).ready(function(){
+
+
+
+
     $('#data').DataTable();
 
-   
-    
- 
 
       var dialog = document.getElementById('myFirstDialog');    
       document.getElementById('show').onclick = function() {    
@@ -13,14 +14,8 @@ $(document).ready(function(){
           dialog.close();    
       };  
 
- 
-      var dialog = document.getElementById('secDialog');    
-      document.getElementById('edit1').onclick = function() {    
-          dialog.show();    
-      }; 
-
-      document.getElementById('hide1').onclick = function() {    
-        dialog.close();    
-    }; 
+      // $('#myModal').on('shown.bs.modal', function () {
+      //   $('#model').trigger('focus')
+      // })
 
 });
